87c202c58d2de7fc1271a69a724e3fc74e9ffe42
Adelmo Filho <adelmo.aguiar.filho@gmail.com>
2018-07-07 19:30:31 -0300

    general updates

--- R/historical_data.R
+++ R/historical_data.R
@@ -1,3 +1,33 @@
+# Retrive historical data per cryptocoin id
+#
+# This function is used to get data from coinmarketcap website
+retrive_data <- function(id){
+
+  Date <- dia <- Open. <- Market.Cap <- Volume <- coin <- NULL
+
+  startdate <- '20000101'
+
+  enddate <- Sys.Date() %>% stri_replace_all_regex("([-])", "")
+
+  url_historical <- paste0('https://coinmarketcap.com/currencies/',id,'/historical-data/?start=',
+                           startdate, '&end=',enddate)
+
+  page_hist <- read_html(url_historical)
+
+  hist_clean <- page_hist %>%
+    html_nodes('table') %>%
+    html_table() %>%
+    data.frame() %>%
+    mutate(dia = format(mdy(Date),"%Y-%m-%d")) %>%
+    mutate(coin = id) %>%
+    select(-Date) %>% select(coin, dia, Open.:Market.Cap) %>%
+    mutate(Volume = suppressWarnings(as.numeric(str_remove_all(Volume, "[,]")))) %>%
+    mutate(Market.Cap = suppressWarnings(as.numeric(str_remove_all(Market.Cap, "[,]"))))
+
+  return(hist_clean)
+
+}
+
 #' Returns a Tidy data frame of historical data for selected cryptocurrencies
 #'
 #' Returns a tidy data frame of historical data for specificed cryptocurrencies
@@ -25,7 +55,6 @@
 #'
 #'
 #' @export
-
 historical_data <- function(cryptocurrency = c('BTC', 'PPC')){
 
   website_slug <- NULL
